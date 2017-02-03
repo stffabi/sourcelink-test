bde5b1d3b73ba4c4aa1f478b7fb58cf71fb4d7d5
Fabrizio Steiner <fas@bbtsoftware.ch>
2017-02-03 08:01:37 +0100

    Initial Commit.

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int TestXy(int value)
+        {
+            if (value == 0)
+            {
+                throw new Exception("Not valid");
+            }
+            return value;
+        }
+    }
+}
